424fa2c127815f5d72c4c6a46a31d7b6df9acba9
John Griffiths <john@uxgent.co>
2015-06-23 09:09:03 -0700

    add image.

--- components/johnantoni/famous-typewriter/famous-typewriter.js
+++ components/johnantoni/famous-typewriter/famous-typewriter.js
@@ -1,17 +1,26 @@
 FamousFramework.scene('johnantoni:famous-typewriter', {
     behaviors: {
-        '#surface': {
+        '#parent': {
             'content': '<h1>Hello johnantoni:famous-typewriter!</h1>',
-            'size': [200, 200],
+            'size': [500, 500],
+            'position': [0, 0],
             'style': {
                 'background-color': 'red',
                 'font-family': 'georgia',
                 'font-size': '0.75rem',
                 'overflow': 'hidden',
-                'word-wrap': 'break-word'
+                'word-wrap': 'break-word',
+                'text-align': 'center',
+                'color': 'white',
+                'padding-bottom': '10px',
+                'border-bottom': '1px solid white'
             }
+        },
+        '#child': {
+          'content': '<div><img src="{{@nostromo.jpg}}"/></div>',
+          'position': [25, 75],
+          'scale': [0.5,0.5]
         }
     },
-
-    tree: '<node id="surface"></node>'
+    tree: '<node id="parent"><node id="child"></node></node>'
 });
