8d1c186f39162ff9094ae873bea3bd698c334ffe
John Griffiths <john@uxgent.co>
2015-06-23 23:55:02 -0700

    Add backticks. strange error on jscs linter in atom - unexpected token ILLEGAL for first line using backticks.

--- components/johnantoni/famous-typewriter/famous-typewriter.js
+++ components/johnantoni/famous-typewriter/famous-typewriter.js
@@ -1,45 +1,49 @@
 FamousFramework.scene('johnantoni:famous-typewriter', {
-    behaviors: {
-        '#parent': {
-            'content': '<h1>Hello johnantoni:famous-typewriter!</h1>',
-            'size': [500, 500],
-            'position': [0, 0],
-            'style': {
-                'background-color': 'red',
-                'font-family': 'georgia',
-                'font-size': '0.75rem',
-                'overflow': 'hidden',
-                'word-wrap': 'break-word',
-                'text-align': 'center',
-                'color': 'white',
-                'padding-bottom': '10px',
-                'border-bottom': '1px solid white'
-            }
-        },
-        '#child': {
-          'content': '<div><img src="{{@nostromo.jpg}}"/></div>',
-          'position': [25, 75],
-          'scale': [0.5,0.5]
-        }
-    },
-    events: {
-        '$lifecycle': {
-            'post-load': function($state) {
-              console.log("page loaded...");
-            }
-        },
-        '#child': {
-            'click': function($state, $payload) {
-                 // get the 'clickCount' value (zero)
-                 var oldValue = $state.get('clickCount');
-                 // increment the counter by 5
-                 $state.set('clickCount', oldValue + 1);
-                console.log('clicked...' + $state.get('clickCount'));
-            }
-        }
-    },
-    states: {
-        clickCount: 0 // will increment by 5 on a click
-    },
-    tree: '<node id="parent"><node id="child"></node></node>'
-});
+  behaviors: {
+      '#parentNode': {
+          'content': '<h1>Hello johnantoni:famous-typewriter!</h1>',
+          'size': [500, 500],
+          'position': [0, 0],
+          'style': {
+              'background-color': 'red',
+              'font-family': 'georgia',
+              'font-size': '0.75rem',
+              'overflow': 'hidden',
+              'word-wrap': 'break-word',
+              'text-align': 'center',
+              'color': 'white',
+              'padding-bottom': '10px',
+              'border-bottom': '1px solid white'
+          }
+      },
+      '#childNode': {
+        'content': '<div><img src="{{@nostromo.jpg}}"/></div>',
+        'position': [25, 75],
+        'scale': [0.5,0.5]
+      }
+  },
+  events: {
+      '$lifecycle': {
+          'post-load': function($state) {
+            console.log("page loaded!...");
+          }
+      },
+      '#childNode': {
+          'click': function($state, $payload) {
+               // get the 'clickCount' value (zero)
+               var oldValue = $state.get('clickCount');
+               // increment the counter by 5
+               $state.set('clickCount', oldValue + 1);
+              console.log('clicked...' + $state.get('clickCount'));
+          }
+      }
+  },
+  states: {
+    clickCount: 0 // will increment by 5 on a click
+  },
+  tree: `
+    <node id='parentNode'>
+      <node id='childNode'></node>
+    </node>
+  `
+})
